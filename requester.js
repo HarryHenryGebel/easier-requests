8ac2273508102a29feae2f4790ecd1292f6dc435
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-11 13:19:31 -0400

    Documentation update - version

--- requester.js
+++ requester.js
@@ -1,9 +1,9 @@
 /**
- * @file provide the requester class
+ * @file Provide the requester class.
  * @author Harry Henry Gebel <hhgebel@gmail.com>
  * @copyright 2020 Harry Henry Gebel
  * @license MIT
- * @version 0.0.0
+ * @version 0.0.1
  * @module easier-requests
  * @since 0.0.0
  */
