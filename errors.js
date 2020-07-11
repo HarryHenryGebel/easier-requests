dc22864802ffd8b9bd4c6f50fcc5116a36f0f936
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-11 13:10:40 -0400

    Create errors.js

---
+++ errors.js
@@ -0,0 +1,10 @@
+/**
+ * @file Provide the error classes
+ * @author Harry Henry Gebel <hhgebel@gmail.com>
+ * @copyright 2020 Harry Henry Gebel
+ * @license MIT
+ * @version 0.0.1
+ * @module easier-requests
+ * @since 0.0.1
+ */
+'use strict';
