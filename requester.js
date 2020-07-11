24140e1be46c85ef0d7735d757035854672e21ce
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-11 01:44:52 -0400

    Start requester.js

---
+++ requester.js
@@ -0,0 +1,8 @@
+/**
+ * @file Project JavaScript file
+ * @author Harry Henry Gebel <hhgebel@gmail.com>
+ * @copyright 2020 Harry Henry Gebel
+ * @license MIT
+ * @version 0.0.0
+ * @module easier-requests
+ */
