c49cfc068c42dd81efed5d384c171b28583730b7
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-11 01:46:11 -0400

    Add initial code

--- requester.js
+++ requester.js
@@ -6,3 +6,30 @@
  * @version 0.0.0
  * @module easier-requests
  */
+'use strict';
+
+class ResponseHolder {
+  constructor() {
+    // holds responses awaiting retrieval
+    this.cachedResponses = {};
+    this.errors = {};
+  }
+
+  // retrieve a response from cache
+  response(id) {
+    const response = this.cachedResponses[id];
+    delete this.cachedResponses[id];
+    return response;
+  }
+
+  // perform an HTTP GET and cache the response
+  async httpGet(url, id) {
+    await axios.get(url)
+      .then(response => this.cachedResponses[id] = response)
+      .catch(function (error) {
+        this.cachedResponses[id] = undefined;
+        this.errors[id] = error;
+      });
+  }
+}
+const cache = new ResponseHolder();
